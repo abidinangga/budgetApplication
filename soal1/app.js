02e937b0c4b16ca1ea25521ff2c7b91369fe6ac6
unknown <abidinangga89@gmail.com>
2022-05-24 11:32:02 +0700

    soal 1 done

--- soal1/app.js
+++ soal1/app.js
@@ -1,12 +1,13 @@
-let num = [3, 2, 4];
-let target = 5;
-let result = [];
-for (let i = 0; i < num.length; i++) {
-  for (let j = 0; j < num.length; j++) {
-    if (num[i] + num[j] == target) {
-      result.push(i, j);
-    }
+let twoSum = (num, target)=> {
+  for (let i = 0; i < num.length; i++) {
+      for (let j = i + 1; j < num.length; j++) {
+          if (num[i] + num[j] === target) {
+            return [i, j];
+          }
+      }
   }
-  break;
 }
-console.log(result);
+
+console.log(twoSum([15, 7, 11, 2],13))
+console.log(twoSum([3, 2, 4],7))
+console.log(twoSum([3, 3],6))
